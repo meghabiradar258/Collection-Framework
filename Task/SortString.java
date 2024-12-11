e94068f6e2a9e5652653b9a7f83ab3b651355c5a
Megha Biradar <meghabiradar589@gmail.com>
2024-12-11 12:49:39 +0530

    Add files via upload

---
+++ Task/SortString.java
@@ -0,0 +1,31 @@
+package Task;
+
+import java.util.*;
+
+public class SortString {
+    public static void main(String[] args) {
+        List<String> name= Arrays.asList("Megha","Anjali","Jaya","Shiva","Arpita","Aai","Shubham","Anuja","Mahesh","Arti");
+        System.out.println(name);
+        List<String>newList=new ArrayList<>();
+
+
+       for(String list1:name){
+        if(list1.toUpperCase().startsWith("A"))
+           {
+               newList.add(list1);
+           }
+
+    }
+
+        Collections.sort(newList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
+        System.out.println("Name Start With A -According to its length");
+        for (String NamesStartWithA : newList) {
+            System.out.println( NamesStartWithA );
+        }
+
+}}
